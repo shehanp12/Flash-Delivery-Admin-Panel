b0a2ac0c461aea7cb21a2569aaeb836cc529ea46
Shehan Prasanna <shehanp068@gmail.com>
2021-02-08 03:18:07 +0530

    auth Actions

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -4,6 +4,8 @@ import runtime from './runtime';
 import navigation from './navigation';
 import posts from './posts';
 import {firestoreReducer} from "redux-firestore";
+import  {getFirebase} from "react-redux-firebase";
+
 
 export default combineReducers({
   auth,
